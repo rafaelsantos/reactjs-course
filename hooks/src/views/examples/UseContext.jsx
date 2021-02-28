24552f7904f2424a8b421bd75575d45f2344f775
santosrafaelbs <rafael.balieiro@luizalabs.com>
2021-02-28 20:55:01 -0300

    Using useContext to manage data context shared for all components

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext from '../../data/DataContext'
 
 const UseContext = (props) => {
+    const context = useContext(DataContext)
+
+    function add(delta) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + delta
+        })
+    }
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
-                subtitle=" Accepts object context and returns context actual value"
-            />
+                subtitle=" Accepts object context and returns context actual value" />
+            
+            <div className="center">
+                <span className="text">{ context.state.text }</span>
+                <span className="text">{ context.state.number }</span>
+
+                <div>
+                    <button className="btn" onClick={ () => add(1) }>+ 1</button>
+                    <button className="btn" onClick={ () => add(-1) }>- 1</button>
+                </div>
+            </div>
         </div>
     )
 }
