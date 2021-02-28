cbd9a43c55ba6449f289c5280392782d4948bfbd
santosrafaelbs <rafael.balieiro@luizalabs.com>
2021-02-28 14:54:34 -0300

    Working with basic navigation

--- navigation/src/components/layout/Content.jsx
+++ navigation/src/components/layout/Content.jsx
@@ -1,9 +1,19 @@
 import './Content.css'
 import React from 'react'
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
+import { Switch, Route } from 'react-router-dom'
 
 const Content = (props) => (
     <main className="Content">
-        Content
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
     </main>
 )
 
--- navigation/src/components/layout/Menu.jsx
+++ navigation/src/components/layout/Menu.jsx
@@ -1,15 +1,17 @@
 import './Menu.css'
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Menu = (props) => (
     <aside className="Menu">
         <nav>
             <ul>
                 <li>
-                    <a href="/">Home</a>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <a href="/">About</a>
+                    <Link to="/about">About</Link>
                 </li>
             </ul>
         </nav>
