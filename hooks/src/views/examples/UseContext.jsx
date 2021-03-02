f248941204b0fc4853ae9a1656a423e09f968767
santosrafaelbs <rafael.balieiro@luizalabs.com>
2021-03-01 21:22:20 -0300

    Using useContext hook to create a store object

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 import DataContext from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
     const context = useContext(DataContext)
+    const { number, setNumber } = useContext(AppContext)
 
     function add(delta) {
         context.setState({
@@ -17,6 +20,7 @@ const UseContext = (props) => {
                 title="Hook UseContext"
                 subtitle=" Accepts object context and returns context actual value" />
             
+            <SectionTitle title="Exercise 01" />
             <div className="center">
                 <span className="text">{ context.state.text }</span>
                 <span className="text">{ context.state.number }</span>
@@ -24,6 +28,15 @@ const UseContext = (props) => {
                 <div>
                     <button className="btn" onClick={ () => add(1) }>+ 1</button>
                     <button className="btn" onClick={ () => add(-1) }>- 1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercise 02" />
+            <div className="center">
+                <span className="text">{ number }</span>
+                <div>
+                    <button className="btn" onClick={ () => setNumber(number + 1) }>+ 1</button>
+                    <button className="btn" onClick={ () => setNumber(number - 1) }>- 1</button>
                 </div>
             </div>
         </div>
